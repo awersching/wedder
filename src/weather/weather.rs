958e2fd9138e0cf389ffddc3ced4f0226803e0a9
Adrian Wersching <dev@awersching.de>
2019-01-07 17:55:40 +0100

    Add parsing of output format

--- src/weather/weather.rs
+++ src/weather/weather.rs
@@ -14,11 +14,29 @@ impl Weather {
     }
 
     pub fn format(&self, format: &String) -> String {
-        format!(
-            "{} {}",
-            self.weather_condition.get_icon(),
-            self.temp_to_celsius(),
-        )
+        let mut formatted = format.clone();
+        formatted = formatted.replace(
+            "<icon>",
+            &*self.weather_condition.get_icon(),
+        );
+
+        if formatted.contains("<temperature_fahrenheit>") {
+            formatted = formatted.replace(
+                "<temperature_fahrenheit>",
+                &*self.temp_to_fahrenheit().to_string(),
+            );
+        } else if formatted.contains("<temperature_kelvin>") {
+            formatted = formatted.replace(
+                "<temperature_kelvin>",
+                &*(self.kelvin.round() as i32).to_string(),
+            );
+        } else {
+            formatted = formatted.replace(
+                "<temperature_celsius>",
+                &*self.temp_to_celsius().to_string(),
+            );
+        }
+        formatted
     }
 
     fn temp_to_celsius(&self) -> i32 {
